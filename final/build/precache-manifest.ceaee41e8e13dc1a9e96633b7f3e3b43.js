d14a04702d2b1e79f142435522b7b46929ad84d0
James Quick <james.q.quick@outlook.com>
2019-11-11 20:30:26 -0600

    Moving final project to it's own folder

---
+++ final/build/precache-manifest.ceaee41e8e13dc1a9e96633b7f3e3b43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489b76d54de4d56bdabf63ddc5df0363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3682aca7cdba0eb09a05",
+    "url": "/static/css/main.281f877a.chunk.css"
+  },
+  {
+    "revision": "7e6f7243a190eee3d1a9",
+    "url": "/static/js/2.4f0e01c0.chunk.js"
+  },
+  {
+    "revision": "3682aca7cdba0eb09a05",
+    "url": "/static/js/main.e2c32337.chunk.js"
+  },
+  {
+    "revision": "ac2cca669e27c1a60ad9",
+    "url": "/static/js/runtime~main.dfd9b444.js"
+  }
+]);
