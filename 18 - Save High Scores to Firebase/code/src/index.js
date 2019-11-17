8e34019c8d680a160b1e1b1a8a9c9b6e55901062
James Quick <james.q.quick@outlook.com>
2019-11-17 13:20:24 -0600

    Moving 18 from HOC for Firebase context to useCOntext hook

--- 18 - Save High Scores to Firebase/code/src/index.js
+++ 18 - Save High Scores to Firebase/code/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import Firebase from './components/Firebase/firebase';
+import { FirebaseContext } from './components/Firebase/context';
 ReactDOM.render(
     <FirebaseContext.Provider value={new Firebase()}>
         <App />
