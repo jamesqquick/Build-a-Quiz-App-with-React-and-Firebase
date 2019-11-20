69dcd582e3be7a3509af31ed1be0095830daf61a
James Quick <james.q.quick@outlook.com>
2019-11-19 19:14:30 -0600

    Updating section 8

--- 8 - Add an Animated Loader/code/src/components/question.js
+++ 8 - Add an Animated Loader/code/src/components/question.js
@@ -3,17 +3,14 @@ import React from 'react';
 export default function Question({ question }) {
     return (
         <div>
-            <h2
-                id="question"
-                dangerouslySetInnerHTML={{ __html: question.question }}
-            />
+            <h2 dangerouslySetInnerHTML={{ __html: question.question }}></h2>
             {question.answerChoices.map((choice, index) => (
                 <div key={index} className="choice-container">
                     <p className="choice-prefix">{index + 1}</p>
                     <p
                         className="choice-text"
                         dangerouslySetInnerHTML={{ __html: choice }}
-                    />
+                    ></p>
                 </div>
             ))}
         </div>
